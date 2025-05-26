c55d66fd6029436053591c81aa2662c5bf7b6648
PFLYIN <pedrodiegomeira@gmail.com>
2025-05-25 22:33:38 -0300

    duas fileiras

--- catalago1.php
+++ catalago1.php
@@ -15,7 +15,7 @@
 <div class="grid">
   <div class="produto" data-id="1" data-nome="VestidoRafaelaRosa" data-preco="149.00"
     data-imagens='["IMG-VESTIDO-RAFAELA/VestidoRosa-FT1.jpg","IMG-VESTIDO-RAFAELA/VESTIDOROSA-FT2.jpg","IMG-VESTIDO-RAFAELA/VestidoRosa-FT3.jpg"]'>
-    <img src="IMG-VESTIDO-RAFAELA/VestidoRosa-FT1.jpg" alt="VestidoRafaela" class="zoom-img">
+    <img src="IMG-VESTIDO-RAFAELA/VestidoRosa-FT1.jpg" alt="Vestido Rosa" class="zoom-img">
     <div class="info">
       <h2>Vestido Rafaela Rosa</h2>
       <p>R$ 149,00</p>
@@ -43,11 +43,11 @@
     </div>
   </div>
 
-  <div class="produto" data-id="2" data-nome="VestidoAmareloIris" data-preco="139.00"
+  <div class="produto" data-id="3" data-nome="VestidoAmareloFloridoIris" data-preco="139.00"
     data-imagens='["IMG-VSTD-IRIS/VstdAmarelo-FT1.jpg","IMG-VSTD-IRIS/VstdAmarelo-FT2.jpg","IMG-VSTD-IRIS/VstdAmarelo-FT3.jpg"]'>
-    <img src="IMG-VSTD-IRIS/VstdAmarelo-FT1.jpg" alt="Vestido Preto" class="zoom-img">
+    <img src="IMG-VSTD-IRIS/VstdAmarelo-FT1.jpg" alt="Vestido Amarelo Florido" class="zoom-img">
     <div class="info">
-      <h2>Vestido Iris Amarelo</h2>
+      <h2>Vestido Amarelo Iris</h2>
       <p>R$ 139,00</p>
       <div class="quantidade">
         <button class="menos">-</button>
@@ -58,11 +58,11 @@
     </div>
   </div>
 
-  <div class="produto" data-id="2" data-nome="VestidoBrancoIris" data-preco="139.00"
+  <div class="produto" data-id="4" data-nome="VestidoBrancoFloridoIris" data-preco="139.00"
     data-imagens='["IMG-VSTD-IRIS/VSTDBranco-FT1.jpg","IMG-VSTD-IRIS/VSTDBranco-FT2.jpg"]'>
-    <img src="IMG-VSTD-IRIS/VSTDBranco-FT1.jpg" alt="Vestido Preto" class="zoom-img">
+    <img src="IMG-VSTD-IRIS/VSTDBranco-FT1.jpg" alt="Vestido Branco Florido" class="zoom-img">
     <div class="info">
-      <h2>Vestido Rafaela Preto</h2>
+      <h2>Vestido Branco Iris</h2>
       <p>R$ 139,00</p>
       <div class="quantidade">
         <button class="menos">-</button>
@@ -73,6 +73,125 @@
     </div>
   </div>
 
+  <div class="produto" data-id="5" data-nome="VestidoVerdeMirian" data-preco="139.00"
+    data-imagens='["Vestidos Mirian/1VsVerdeMirian.jpg","Vestidos Mirian/2VsVerdeMirian.jpg","Vestidos Mirian/3VerdeMirian.jpg"]'>
+    <img src="Vestidos Mirian/1VsVerdeMirian.jpg" alt="Vestido Verde" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Mirian Verde</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="6" data-nome="VestidoLilasMirian" data-preco="139.00"
+    data-imagens='["Vestidos Mirian/1LilasMirian.jpg","Vestidos Mirian/2LilasMirian.jpg","Vestidos Mirian/3LilasMirian.jpg"]'>
+    <img src="Vestidos Mirian/1LilasMirian.jpg" alt="Vestido Lilas" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Mirian Lilas</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="7" data-nome="VestidoBegeMirian" data-preco="139.00"
+    data-imagens='["Vestidos Mirian/1LilasMirian.jpg","Vestidos Mirian/2LilasMirian.jpg","Vestidos Mirian/3LilasMirian.jpg"]'>
+    <img src="Vestidos Mirian/1LilasMirian.jpg" alt="Vestido Bege" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Mirian Bege</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="8" data-nome="VestidoVinhoLaís" data-preco="139.00"
+    data-imagens='["Vestidos Laís/1VsVinhoLaís.jpg","Vestidos Laís/2VsVinhoLaís.jpg","Vestidos Laís/3VsVinhoLaís.jpg"]'>
+    <img src="Vestidos Laís/1VsVinhoLaís.jpg" alt="Vestido Vinho" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Laís Vinho</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="9" data-nome="VestidoPretoLaís" data-preco="139.00"
+    data-imagens='["Vestidos Laís/1VSPRETOLAÍS.jpg","Vestidos Laís/2VSPRETOLAÍS.jpg","Vestidos Laís/3VSPRETOLAÍS.jpg"]'>
+    <img src="Vestidos Laís/1VSPRETOLAÍS.jpg" alt="Vestido Preto" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Laís Preto</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="10" data-nome="VestidoTerracotaLaís" data-preco="139.00"
+    data-imagens='["Vestidos Laís/1TerracotaLaís.jpg","Vestidos Laís/2TerracotaLaís.jpg","Vestidos Laís/3Terracota.jpg"]'>
+    <img src="Vestidos Laís/1TerracotaLaís.jpg" alt="Vestido Terracota" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Laís Terracota</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="11" data-nome="VestidoBrancoKarol" data-preco="139.00"
+    data-imagens='["Vestidos Karol/1VsBrancoFloridoKarol.jpg","Vestidos Karol/2VsBrancoKarol.jpg","Vestidos Karol/3VsBrancoKarol.jpg"]'>
+    <img src="Vestidos Karol/1VsBrancoFloridoKarol.jpg" alt="Vestido Branco Florido" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Karol Branco</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
+
+  <div class="produto" data-id="12" data-nome="VestidoAmareloKarol" data-preco="139.00"
+    data-imagens='["Vestidos Karol/1AMARELOKarol.jpg","Vestidos Karol/2AMARELOKarol.jpg","Vestidos Karol/3AMARELOKarol.jpg"]'>
+    <img src="Vestidos Karol/1VsBrancoFloridoKarol.jpg" alt="Vestido Amarelo Florido" class="zoom-img">
+    <div class="info">
+      <h2>Vestido Karol Amarelo</h2>
+      <p>R$ 139,00</p>
+      <div class="quantidade">
+        <button class="menos">-</button>
+        <span class="qtd">1</span>
+        <button class="mais">+</button>
+      </div>
+      <button class="add-carrinho">🛒 + Adicionar</button>
+    </div>
+  </div>
 
  
 
